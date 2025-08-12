574066324694c45a942bf0c6fee9f77511fbbaa7
happypanda2222 <tripods.scrawls1n@icloud.com>
2025-08-12 04:37:11 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ac-pay-v1-2';
+const CACHE = 'ac-pay-v1-3';
 const ASSETS = [
   './',
   './index.html',
@@ -9,10 +9,14 @@ const ASSETS = [
   './icons/apple-touch-icon-180.png'
 ];
 self.addEventListener('install', e=>{
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
 self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  e.waitUntil(Promise.all([
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
+    self.clients.claim()
+  ]));
 });
 self.addEventListener('fetch', e=>{
   e.respondWith(caches.match(e.request).then(res=>res || fetch(e.request)));
